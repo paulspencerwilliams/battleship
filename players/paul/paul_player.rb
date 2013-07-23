242553985473de6827df7a346785bf1d9face2d1
Paul Williams <paul@paulswilliams.me.uk>
2013-07-23 13:00:01 +0100

    simplified interface to strategy

--- players/paul/paul_player.rb
+++ players/paul/paul_player.rb
@@ -1,12 +1,14 @@
-
 class PaulPlayer
+  def initialize(strategy = Strategy.new, last_turn = LastTurn.new)
+    @strategy = strategy
+    @last_turn = last_turn
+  end
 
   def name
     "Paul"
   end
 
   def new_game
-    @memory.forget_previous_game
     [
       [0, 0, 5, :down],
       [4, 4, 4, :across],
@@ -17,6 +19,9 @@ class PaulPlayer
   end
 
   def take_turn(state, ships_remaining)
+    suggestion = @strategy.suggest_next(@last_turn, state)
+    @last_turn.remember_turn(suggestion)
+    suggestion
   end
 
 end
--- spec/players/paul/paul_player_spec.rb
+++ spec/players/paul/paul_player_spec.rb
@@ -2,8 +2,46 @@ require "spec_helper"
 
 describe PaulPlayer do
   describe "when asked for name" do
-    it "returns Paul" do
+    xit "returns Paul" do
       subject.name.should eq("Paul")
     end
   end
+
+  describe "when informed to start a new game" do
+    xit "it should use appropriate strategy for placing ships" do
+    end
+
+  end
+
+  describe "when invited to take turn" do
+    let (:strategy)        {double("strategy")}
+    let (:suggested_coord) {double("suggested coord")}
+    let (:last_turn)       {double("last_turn")}
+    let (:state)           {double("state")}
+    let (:ships_remaining) {double("ships remaining")}
+
+    subject {PaulPlayer.new(strategy, last_turn)}
+
+    before (:each) do
+      strategy.stub(:suggest_next).and_return(suggested_coord)
+      last_turn.stub(:remember_turn) 
+    end
+
+
+    it "should ask strategy for suggestion based on last turn" do
+      strategy.should_receive(:suggest_next).with(last_turn, state)
+      subject.take_turn(state, ships_remaining)
+    end
+
+    it "should return the suggested coord" do
+      subject.take_turn(state, ships_remaining).should eq(suggested_coord)
+    end
+
+    it "should remember turn" do
+      last_turn.should_receive(:remember_turn).with(suggested_coord)
+      subject.take_turn(state, ships_remaining)
+    end
+
+  end
 end
+
